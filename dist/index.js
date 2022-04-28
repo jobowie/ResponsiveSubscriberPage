18a9925595473bdcd111325422d7276086da8d6f
jobowie <60718310+jobowie@users.noreply.github.com>
2022-04-28 01:58:47 -0500

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+const openBtn = document.querySelector('button');
+const closeBtn = document.querySelector('#close');
+const modal = document.querySelector('.modal');
+const form = document.querySelector('form');
+
+openBtn.addEventListener('click', openModal);
+
+function openModal() {
+  modal.style.display = 'grid'
+}
+
+closeBtn.addEventListener('click', closeModal); 
+function closeModal() {
+  modal.style.display = 'none';
+  modal.classList.add('close-modal-animation');
+  setTimeout(() => {
+    modal.style.display = 'none';
+    window.location.reload();
+  }, 1000); 
+}
+
+modal.addEventListener('click', function (e) {
+  if (e.target.classList.contains('modal')) {
+    closeModal();
+  }
+})
+
+form.addEventListener('submit', submitForm);
+
+function submitForm() {
+  const card = documentquerySelector('.card');
+  card.innerHTML = "<h1>Welcome to the Team!</h1>";
+  setTimeout(() => {
+    closeModal();
+  }, 3000);
+}
+
+
+
